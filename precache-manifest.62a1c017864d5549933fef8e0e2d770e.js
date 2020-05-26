b4bc796f37b931ed417a8d376676be54eb9822e6
WMV1019 <willyan.marques@accenture.com>
2020-05-26 02:00:37 -0300

    Updates

---
+++ precache-manifest.62a1c017864d5549933fef8e0e2d770e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdba23e5e043c42ada062753c4d840fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d0b2c3488b4ddd4efc6",
+    "url": "./static/css/main.c4bec5bf.chunk.css"
+  },
+  {
+    "revision": "ee49ca58a03de96678c3",
+    "url": "./static/js/2.804aa1ea.chunk.js"
+  },
+  {
+    "revision": "57f43f160134c6feaaa84b1e4de266b4",
+    "url": "./static/js/2.804aa1ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0b2c3488b4ddd4efc6",
+    "url": "./static/js/main.bed9bd6c.chunk.js"
+  },
+  {
+    "revision": "3c5697d302c4a89c2118",
+    "url": "./static/js/runtime-main.81911968.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3661fb6e65751020cba93c73130644f0.js"
+  "./precache-manifest.62a1c017864d5549933fef8e0e2d770e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

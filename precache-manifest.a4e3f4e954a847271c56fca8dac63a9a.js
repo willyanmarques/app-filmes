c6a0ee203f1d82227c7b2279b2f6c26bc874879a
WMV1019 <willyan.marques@accenture.com>
2020-05-26 09:39:56 -0300

    Updates

---
+++ precache-manifest.a4e3f4e954a847271c56fca8dac63a9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e692ed4415c0745c057603a04892301",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f1ce5c63b35fbf64e89",
+    "url": "/static/css/main.c4bec5bf.chunk.css"
+  },
+  {
+    "revision": "ee49ca58a03de96678c3",
+    "url": "/static/js/2.804aa1ea.chunk.js"
+  },
+  {
+    "revision": "57f43f160134c6feaaa84b1e4de266b4",
+    "url": "/static/js/2.804aa1ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1ce5c63b35fbf64e89",
+    "url": "/static/js/main.43221f01.chunk.js"
+  },
+  {
+    "revision": "e94376d2f99a4d416af5",
+    "url": "/static/js/runtime-main.d3549a5e.js"
+  }
+]);
